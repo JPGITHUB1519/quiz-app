f0b275270a9ff8f450812718f6ba799a93ca8495
Jean Urena <juanpedro1519@gmail.com>
2021-02-14 12:01:15 -0400

    deploy commit 2021-02-14_12:01:15

--- commit-deploy.bash
+++ commit-deploy.bash
@@ -3,11 +3,13 @@
 # Remember commiting all your changes before deployment
 
 # Run the webpack production build
-# npm run build
+npm run build
 
 # make git deploy commit
-# git add .
-# git commit -m "deploy commit $(date +%Y-%m-%d_%T)"
+git add .
+
+# commiting with timestamps
+git commit -m "deploy commit $(date +%Y-%m-%d_%T)"
 
 # Deploy dist folder to github pages from the development or master branch
 git subtree push --prefix dist origin gh-pages
