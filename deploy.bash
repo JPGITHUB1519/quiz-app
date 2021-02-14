81e5290edb24bd1161eaef71e8a1bdb381f131aa
Jean Urena <juanpedro1519@gmail.com>
2021-02-13 23:49:55 -0400

    deploy commit 2021-02-13_23:49:55

--- deploy.bash
+++ deploy.bash
@@ -7,7 +7,7 @@ npm run build
 
 # make git deploy commit
 git add .
-git commit -m "deploy commit $(date +%Y-%m-%d)"
+git commit -m "deploy commit $(date +%Y-%m-%d_%T)"
 
 # Deploy dist folder to github pages from the development or master branch
 git subtree push --prefix dist origin gh-pages
